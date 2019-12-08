0454ee98f50f2331db312bb9c2fa49656d3b99bb
ChaeByunghoon <cbh1203@naver.com>
2019-12-08 15:12:51 +0900

    first commit

---
+++ src/main/java/com/example/makeapi/MakeapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.makeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeapiApplication.class, args);
+	}
+
+}
